6782e049d0e09bd24998ac472a3d6be9381ae092
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2026-05-20 14:31:57 +0000

    modifica método self_train da classe MENACE

--- menace_jogo/files/api.py
+++ menace_jogo/files/api.py
@@ -422,35 +422,46 @@ class Jogador:
         self.jogadas = []
         self.num_jogos += 1
 
-    def self_train(self, num_jogos, versus_dummy=True):
+    def self_train(self, num_jogos, strategy="dummy"):
         """Treina o MENACE fazendo ele jogar contra outro MENACE."""
 
         oponente_num = 1 if self.player_num == 2 else 2
 
-        if versus_dummy:
-            adversario = Jogador(
-                oponente_num,
-                reforco_vitoria=0,
-                reforco_derrota=0,
-            )
-        else:
-            adversario = Jogador(oponente_num)
+        match strategy.lower():
+            case "dummy":
+                adversario = Jogador(
+                    oponente_num,
+                    reforco_vitoria=0,
+                    reforco_derrota=0,
+                )
+
+            case "slow learner":
+                adversario = Jogador(
+                    oponente_num,
+                    reforco_vitoria=0.1,
+                    reforco_derrota=0.1,
+                )
+
+            case _:
+                adversario = Jogador(oponente_num)
 
         if self.player_num == 1:
-            self, adversario, _, _, _ = simulacao(
+            self, adversario, vitorias, derrotas, empates = simulacao(
                 self,
                 adversario,
                 num_jogos,
             )
 
         else:
-            adversario, self, _, _, _ = simulacao(
+            adversario, self, vitorias, derrotas, empates = simulacao(
                 adversario,
                 self,
                 num_jogos,
             )
 
         print(f"Máquina treinada com sucesso. {num_jogos} jogos.")
+
+        return vitorias, derrotas, empates, adversario
 
 
 def simulacao(player1, player2, num_jogos=100):
